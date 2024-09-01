373dbd7de4699493bfb2484c0a60938aa18ca6c0
Edgard Carrillo <ea_carrillo@hotmail.com>
2024-09-01 09:05:14 -0500

    Update01Sep06

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:8000", 
-    "https://marcobremantenimientobpy-production.up.railway.app/",
+    "https://marcobremantenimientobpy-production.up.railway.app",
 ]
 
 app.add_middleware(
