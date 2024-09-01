36cfeb48c09eaefbb08b5f4913ffd50ea620298b
Edgard Carrillo <ea_carrillo@hotmail.com>
2024-09-01 08:24:19 -0500

    Update01Sep00

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from api.v1.costos import router as v1_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000", 
+    "https://marcobremantenimientobpy-production.up.railway.app/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
 app.include_router(v1_router,prefix="/api/v1")
 
